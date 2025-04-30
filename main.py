3f1f68db475b403efd3f56a6d3ccc219c9f2755e
rayyush <142962272+rayyush@users.noreply.github.com>
2025-04-30 13:00:19 +0000

    creating the major outline of functions

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from tkinter import *
-import calender
+import calendar
 
+def Calender_See():
+    pass
+
+if __name__ == '__main__':
+    root = Tk() 
+    root.config(background="red")
+    root.title("GUI Calender")
+    root.geometry("250x180")
+
+    name = Label(root, text="Calendar", bg="Light green", font=("Arial", 20, "bold")) 
+    name.grid(row=1, column = 1)
+
+    root.mainloop()
