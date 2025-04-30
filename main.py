1df03478c0c0f7aa1024bcf7f2fc77448f67df0e
rayyush <142962272+rayyush@users.noreply.github.com>
2025-04-30 13:43:45 +0000

    basic ui change

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from tkinter import *
 import calendar
 
 def Calender_See():
-    pass
+    window = Tk()
+    
 
 if __name__ == '__main__':
     root = Tk() 
@@ -10,7 +11,13 @@ if __name__ == '__main__':
     root.title("GUI Calender")
     root.geometry("250x180")
 
-    name = Label(root, text="Calendar", bg="Light green", font=("Arial", 20, "bold")) 
+    name = Label(root, text="Calendar", bg="light green", font=("Arial", 20, "bold")) 
     name.grid(row=1, column = 1)
+    year = Label(root, text="Enter the year", bg="light blue", font=("Arial", 18, "bold"))
+    year.grid(row = 2, column = 2)
+    year_entry = Entry(root, font=("Arial", 15, "bold"))
+    year_entry.grid(row = 3, column = 3)
+    show_button = Button(root, text = "Show Calender!", fg = "black", bg = "white", font=("Arial", 20, "bold"), command=Calender_See)
+
 
     root.mainloop()
